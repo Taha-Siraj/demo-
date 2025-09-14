72480349461bc64e80d3269bdb2990d8f54551cd
Taha-siraj <pikachugaming899@gmail.com>
2025-09-14 11:01:35 -0700

    add a code

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
 import express from 'express';
 import { configDotenv } from 'dotenv';
 import cors from 'cors';
+import mongoose from 'mongoose';
+import router from './routes/contact.js';
 
 const app = express();
 configDotenv();
+
 app.use(cors());
 app.use(express.json());
 
+// MongoDB connect
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB is connected"))
+  .catch((error) => console.log(error, "MongoDB is not connected"));
 
+// Test route
 app.get('/', (req, res) => {
-    res.send("api is working")
-})
+  res.send("API is working ✅");
+});
 
-app.listen(5002 , () => {
-    console.log("server is running port 5002")
-})
+// Routes
+app.use('/api', router);
+
+app.listen(5002, () => {
+  console.log("Server is running on port 5002");
+});
---
+++ routes/contact.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import Contact from '../models/ContactDB.js';
+
+const router = express.Router();
+
+router.get("/services", async (req, res) => {
+    try {
+        let allservices = await Contact.find()
+        res.status(200).send(allservices)
+    } catch (error) {
+        res.status(400).send("server error")
+        console.log(error)
+    }
+})
+
+
+router.post("/Addservices", async (req, res) => {
+    try {
+        let allservices = await Contact.find()
+        res.status(200).send(allservices)
+    } catch (error) {
+        res.status(400).send("server error")
+        console.log(error)
+    }
+})
+
+export default router;
+
